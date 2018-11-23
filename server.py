0ad1bcfa2057a768e37e119e84d78899c097253a
Alexandra Espichan Linares <alexandra.espichan@rimac.com.pe>
2018-11-23 02:16:45 -0500

    texto estandar

--- server.py
+++ server.py
@@ -69,7 +69,7 @@ class Employees(Resource):
         else:
             msg += " de " + hotel
 
-        msg += " en"+month_name+" del "+str(cut_day.year)+" al "+cut_day.strftime('%d/%m/%Y')+": "
+        msg += " en "+month_name+" del "+str(cut_day.year)+" al "+cut_day.strftime('%d/%m/%Y')+": "
 
         cut_day = cut_day.strftime('%d/%m/%Y')
         init_day = init_day.strftime('%d/%m/%Y')
