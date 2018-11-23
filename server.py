f2587b10750bd50a447278229b7617f87c3db79c
Alexandra Espichan Linares <alexandra.espichan@rimac.com.pe>
2018-11-23 01:38:31 -0500

    texto estandar

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 from json import dumps
 import pymssql
-from datetime import datetime
 
 conn = pymssql.connect(server = 'brein.c75yaa9tpxts.us-east-2.rds.amazonaws.com', user = 'BREIN', password = 'Rachel123456')
 cursor = conn.cursor()
@@ -10,32 +9,30 @@ cursor = conn.cursor()
 app = Flask(__name__)
 api = Api(app)
 
-
-
 class Employees(Resource):
     def post(self):
         req = request.get_json(silent = True, force = True)
         params = req.get('queryResult').get('parameters')
         metric = params.get('metric')
         hotel = params.get('company')
+        periodo = params.get('period')
 
-        periodo = params.get('company')
+        msg = metric
 
         if not hotel:
-            hotel = 'ALL' 
+            hotel = 'ALL'
+            msg += " general: "
+        else:
+            msg += " de " + hotel +": "
             
-        print('******************************************')
-        print(hotel)
-        print('******************************************')
-
         segment = 'RETAIL'
         query = "SELECT BREIN.DBO.PRUEBA_4('20/11/2018','01/11/2018','30/11/2018','%s','%s')" % (segment, hotel)
         cursor.execute(query)
         row = cursor.fetchone()
 
-        msg = 'El ' + metric + ' es ' + str(row[0])
+        msg += str(row[0])
         response = {
-        "fulfillmentText": msg
+            "fulfillmentText": msg
         }
 
         return make_response(jsonify(response))
